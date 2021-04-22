eb007e352d4691ecd3f6ca17b632a952bedc958e
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-04-21 22:04:43 -0700

    feat(tools): root tests
    Add tests for the root app component.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: any;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, FooterComponent, NavComponent],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create the app', () => {
@@ -20,5 +29,26 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('dennagrey');
+  });
+
+  it('should render the nav component', () => {
+    const nav = compiled.querySelector('app-nav');
+    expect(nav).toBeDefined();
+  });
+
+  it('should render the router outlet', () => {
+    const router = compiled.querySelector('router-outlet');
+    expect(router).toBeDefined();
+  });
+
+  it('should render the footer component', () => {
+    const footer = compiled.querySelector('app-footer');
+    expect(footer).toBeDefined();
+  });
+
+  it('should render the components in order', () => {
+    expect(compiled.children[0].tagName).toBe('APP-NAV');
+    expect(compiled.children[1].tagName).toBe('ROUTER-OUTLET');
+    expect(compiled.children[2].tagName).toBe('APP-FOOTER');
   });
 });
